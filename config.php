60e993b68122d51120079657b9af35896689d981
albertrono <alberitorono@gmail.com>
2022-06-20 09:02:31 +0300

    initial commit

---
+++ config.php
@@ -0,0 +1,36 @@
+<?php
+	$dbServer = 'localhost';
+	$dbUsername = 'root';
+	$dbPassword = '';
+	$dbDatabase = 'fantastic_school_admin_db';
+
+	$adminConfig = array(
+		'adminUsername' => "admin",
+		'adminPassword' => "50149f15229fc3562a31e98f31f4b387",
+		'notifyAdminNewMembers' => "0",
+		'defaultSignUp' => "1",
+		'anonymousGroup' => "anonymous",
+		'anonymousMember' => "guest",
+		'groupsPerPage' => "10",
+		'membersPerPage' => "10",
+		'recordsPerPage' => "10",
+		'custom1' => "",
+		'custom2' => "",
+		'custom3' => "",
+		'custom4' => "",
+		'MySQLDateFormat' => "%m/%d/%Y",
+		'PHPDateFormat' => "n/j/Y",
+		'PHPDateTimeFormat' => "m/d/Y, h:i a",
+		'senderName' => "Membership management",
+		'senderEmail' => "alberitorono@gmail.com",
+		'approvalSubject' => "Your membership is now approved",
+		'approvalMessage' => "Dear member,\r\n\r\nYour membership is now approved by the admin. \r\n\r\nRegards,\r\nAdmin",
+		'hide_twitter_feed' => "",
+		'maintenance_mode_message' => "<b>Our website is currently down for maintenance</b><br>\r\nWe expect to be back in a couple hours. Thanks for your patience.",
+		'mail_function' => "mail",
+		'smtp_server' => "",
+		'smtp_encryption' => "",
+		'smtp_port' => "25",
+		'smtp_user' => "",
+		'smtp_pass' => ""
+	);
